9e7d254aa7bb104a7c457dc1415ad646ef6fb0d2
Marcus Posey <marcusposey@gmail.com>
2016-12-16 15:12:33 -0600

    Create list type declarations

--- list/list.go
+++ list/list.go
@@ -1,1 +1,21 @@
 package list
+
+// Object defines an interface for List values.
+//
+// All inserted values should implement the operations defined here.
+type Object interface {
+	// Return true if other is less than this.
+	Less(other Object) bool
+}
+
+// ListNode is a node for a List.
+type ListNode struct {
+	object      *Object
+	left, right *ListNode
+}
+
+// List is a double-linked list.
+// TODO: Improve List documentation.
+type List struct {
+	head, tail *ListNode
+}
