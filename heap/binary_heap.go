274d25575816f86d81172bf942dae7a882f151cc
Marcus Posey <marcusposey@gmail.com>
2016-12-16 20:43:44 -0600

    Implement NewHeap

--- heap/binary_heap.go
+++ heap/binary_heap.go
@@ -2,7 +2,7 @@ package heap
 
 // Heap implements a binary heap.
 //
-// KeyCompare -
+// Comparator -
 //	This required function compares two heap objects. The return value
 //	should be:
 //		-1 if arg1 < arg2
@@ -10,7 +10,7 @@ package heap
 //		1  if arg1 > arg2
 type Heap struct {
 	// Compare two heap objects
-	KeyCompare func(interface{}, interface{}) int8
+	Comparator func(interface{}, interface{}) int8
 
 	// The max number of items Heap can hold before resizing
 	Capacity uint
@@ -19,4 +19,24 @@ type Heap struct {
 	Size uint
 
 	items []interface{}
+}
+
+// NewHeap creates a new *Heap.
+//
+// size - the initial capacity of the Heap
+//	  If not supplied, a default capacity is chosen.
+//
+// see Heap doc for valid comparator definitions.
+func NewHeap(comparator func(interface{}, interface{}) int8, capacity ...uint) *Heap {
+	var cap uint
+	if len(capacity) == 0 {
+		cap = 20
+	} else {
+		cap = capacity[0]
+	}
+	return &Heap{
+		Comparator: comparator,
+		Capacity:   cap,
+		items:      make([]interface{}, cap),
+	}
 }
--- heap/binary_heap_test.go
+++ heap/binary_heap_test.go
@@ -1,1 +1,22 @@
 package heap
+
+import "testing"
+
+func TestNewHeap(t *testing.T) {
+	h := NewHeap(func(a, b interface{}) int8 {
+		if a.(int) < b.(int) {
+			return -1
+		} else if a.(int) == b.(int) {
+			return 0
+		} else {
+			return 1
+		}
+	})
+
+	if h.Capacity != 20 {
+		t.Error("Wrong initial capacity of Heap")
+	}
+	if len(h.items) != int(h.Capacity) {
+		t.Error("Could not initialize heap slice")
+	}
+}
