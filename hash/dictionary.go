27d54eb7e1b836b0abc1dfa0647a2186fe4e0426
Marcus Posey <marcusposey@gmail.com>
2016-12-20 15:09:33 -0600

    Create base NewDictionary function

--- hash/dictionary.go
+++ hash/dictionary.go
@@ -1,5 +1,7 @@
 package hash
 
+import "math"
+
 // Dictionary is a hash table for large volume data sets.
 //
 // If you expect the number of stored elements will be small, consider using
@@ -10,6 +12,36 @@ package hash
 // TODO: Mention Hashable interface
 type Dictionary struct {
 	table []uint
+
+	// The number of possble hash values
+	// tableSize should always be a power of 2. This allows us to avoid
+	// mods in favor of ands.
+	tableSize uint
+}
+
+// NewDictionary creates an empty Dictionary with an optional table size.
+//
+// tableSize should be a power of two. If it is not, a higher tableSize
+// will be chosen which is a power of two.
+func NewDictionary(tableSize ...uint) *Dictionary {
+	cap := 8
+	if len(tableSize) != 0 {
+		// Make capacity a power of two.
+		cap = math.Exp2(math.Ceil(math.Log2(tableSize[0])))
+	}
+	return cap
+}
+
+func (d *Dictionary) Insert(key interface{}, object interface{}) error {
+	return nil
+}
+
+func (d *Dictionary) Get(key interface{}) interface{} {
+	return nil
+}
+
+func (d *Dictionary) Remove(key interface{}) error {
+	return nil
 }
 
 // hashKey hashes the key to a uint.
@@ -28,18 +60,6 @@ func hashKey(key interface{}) uint {
 	default:
 		return key.(Hashable).Hash()
 	}
-}
-
-func (d *Dictionary) Insert(key interface{}, object interface{}) error {
-	return nil
-}
-
-func (d *Dictionary) Get(key interface{}) interface{} {
-	return nil
-}
-
-func (d *Dictionary) Remove(key interface{}) error {
-	return nil
 }
 
 // Hashable is an interface for objects which can compute their hash values.
